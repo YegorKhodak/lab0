fb69b83263b9090c6848edca9ce2cf7c1c7c5644
YegorKhodak <43115440+YegorKhodak@users.noreply.github.com>
2019-10-10 18:42:26 +0300

    initial

---
+++ labo.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np;camera = cv2.VideoCapture(0)
+
+def lab0():
+    return_value, image = camera.read()
+    cv2.imwrite('opencv.png', image)
+    img = cv2.imread('opencv.png')
+    cv2.imshow('res', img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.line(img, (0, 0), (img.shape[1], img.shape[0]) , (100, 0, 0), 4)
+    cv2.rectangle(img, (50, 50), (100, 150), (100, 100, 255), 2)
+    cv2.imwrite('opencv.png', img)
+    cv2.imshow('res', img)
+
+
+if __name__ == "__main__":
+    lab0()
