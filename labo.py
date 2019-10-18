889e3c2e586ffd4969f9343454976495ddf289ab
YegorKhodak <43115440+YegorKhodak@users.noreply.github.com>
2019-10-18 17:13:56 +0300

    Update labo.py

--- labo.py
+++ labo.py
@@ -7,6 +7,7 @@ def lab0():
     img = cv2.imread('opencv.png')
     cv2.imshow('res', img)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
     cv2.line(img, (0, 0), (img.shape[1], img.shape[0]) , (100, 0, 0), 4)
     cv2.rectangle(img, (50, 50), (100, 150), (100, 100, 255), 2)
     cv2.imwrite('opencv.png', img)
