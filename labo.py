becc73aa56f3dc5a3606c4c6f94a0b3c5c604a08
YegorKhodak <43115440+YegorKhodak@users.noreply.github.com>
2019-12-25 11:59:01 +0200

    mm

--- labo.py
+++ labo.py
@@ -1,17 +1,20 @@
 import cv2
-import numpy as np;camera = cv2.VideoCapture(0)
+import numpy as np
 
 def lab0():
-    return_value, image = camera.read()
-    cv2.imwrite('opencv.png', image)
-    img = cv2.imread('opencv.png')
-    cv2.imshow('res', img)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    cv2.line(img, (0, 0), (img.shape[1], img.shape[0]) , (100, 0, 0), 4)
-    cv2.rectangle(img, (50, 50), (100, 150), (100, 100, 255), 2)
-    cv2.imwrite('opencv.png', img)
-    cv2.imshow('res', img)
+    camera = cv2.VideoCapture(0)
+    ret, image = camera.read()
+    cv2.imwrite('cameraShot.png', image)
+    camera.release()
+    cv2.imshow('img', image)
+    image = cv2.imread('cameraShot.png')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.cvtColor(image, cv2.COLOR_GRAY2BDR)
+    cv2.line(image, (0, 0), (image.shape[1], image.shape[0]) , (100, 0, 0), 5)
+    cv2.rectangle(image, (50, 50), (100, 150), (100, 100, 0), 5)
+    cv2.imwrite('cameraShot.png', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
